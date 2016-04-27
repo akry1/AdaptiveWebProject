9d6ca554b34c6d6be1a6338738d0f30db1461a2b
Aravinda Kumar Reddy Yempada <Aravinda Kumar Reddy Yempada>
2016-04-27 01:12:18 -0700

    added remaining files

---
+++ SQLQuery3.sql
@@ -0,0 +1,4 @@
+﻿insert into AspNetUsers (Id, Email, UserName, PasswordHash, SecurityStamp, EmailConfirmed, PhoneNumberConfirmed,TwoFactorEnabled,LockoutEnabled, AccessFailedCount) values 
+( '2858871','2858871', '2858871' , 'AGVUTEm/eYCUV0bNHQWJZL6BEea5PG/8as9uMAJXdSPvhAostSaEQIFHz0jBb6iaLg==', '68348624-3041-4de0-a230-17b4eeb35c13', 0,0,0,0,0)
+
+
