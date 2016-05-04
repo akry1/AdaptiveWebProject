e4cce07beb92523695015829b41832a9b3319451
Aravinda Yempada <aravind_yempada@outlook.com>
2016-05-04 01:17:14 -0700

    Pie chart title

--- AdaptiveWebProject/Controllers/HomeController.cs
+++ AdaptiveWebProject/Controllers/HomeController.cs
@@ -13,7 +13,7 @@ using System.Data.Entity;
 
 namespace AdaptiveWebProject.Controllers
 {
-    
+
     public class HomeController : Controller
     {
 
@@ -47,7 +47,8 @@ namespace AdaptiveWebProject.Controllers
         {
             List<String> questions = new List<String>();
 
-            try {
+            try
+            {
                 var id = User.Identity.GetUserId();
 
                 var userdata = db.UsersData.Where(a => a.UserId.Contains(id));
@@ -69,59 +70,110 @@ namespace AdaptiveWebProject.Controllers
 
                 List<PostWithDifficulty> postdetails = new List<PostWithDifficulty>();
                 string userExpertise = "";
-                int maxPoints = 0,pointsMod = 0;
-                switch (expertise[0])
-                {
-                    case 1:
-                        userExpertise = "Naive";
-                        //minPoints = 0;
-                        maxPoints = 100;
-                        pointsMod = points[0];
-                        //postdetails = db.PostDetails.Join(db.Posts, a => a.PostId, b => b.PostId, (a, b) => new PostWithDifficulty { Difficulty = a.Difficulty, Tags = b.Tags, Question = b.Question }).Where(a => a.Difficulty.Contains("Easy")).ToList();
-                        postdetails = dbView.EasyPosts.Select(a => new PostWithDifficulty { PostId = a.PostId, Tags = a.Tags, Question = a.Question }).ToList();
-                        break;
-                    case 2:
-                        userExpertise = "Mediocre";
-                        maxPoints = 400;
-                        pointsMod = points[0] - 100;
-                        //postdetails = db.PostDetails.Join(db.Posts, a => a.PostId, b => b.PostId, (a, b) => new PostWithDifficulty { Difficulty = a.Difficulty, Tags = b.Tags, Question = b.Question }).Where(a => a.Difficulty.Contains("Moderate")).ToList();
-                        postdetails = dbView.ModeratePosts.Select(a => new PostWithDifficulty { PostId = a.PostId, Tags = a.Tags, Question = a.Question }).ToList();
-                        break;
-                    case 3:
-                        userExpertise = "Expert";
-                        maxPoints = 10000;
-                        if (points[0] > 10000)
-                            pointsMod = 10000;
-                        else
-                            pointsMod = points[0] - 500;
-                        //postdetails = db.PostDetails.Join(db.Posts, a => a.PostId, b => b.PostId, (a, b) => new PostWithDifficulty { Difficulty = a.Difficulty, Tags = b.Tags, Question = b.Question }).Where(a => a.Difficulty.Contains("Hard")).ToList();
-                        postdetails = dbView.HardPosts.Select(a => new PostWithDifficulty { PostId = a.PostId, Tags = a.Tags, Question = a.Question }).ToList();
-                        break;
-                }
+                int maxPoints = 0, pointsMod = 0;
+
+                //switch (expertise[0])
+                //{
+                //    case 1:
+                //        userExpertise = "Naive";
+                //        //minPoints = 0;
+                //        maxPoints = 100;
+                //        pointsMod = points[0];
+                //        //postdetails = db.PostDetails.Join(db.Posts, a => a.PostId, b => b.PostId, (a, b) => new PostWithDifficulty { Difficulty = a.Difficulty, Tags = b.Tags, Question = b.Question }).Where(a => a.Difficulty.Contains("Easy")).ToList();
+                //        postdetails = dbView.EasyPosts.Select(a => new PostWithDifficulty { PostId = a.PostId, Tags = a.Tags, Question = a.Question }).ToList();
+                //        break;
+                //    case 2:
+                //        userExpertise = "Mediocre";
+                //        maxPoints = 400;
+                //        pointsMod = points[0] - 100;
+                //        //postdetails = db.PostDetails.Join(db.Posts, a => a.PostId, b => b.PostId, (a, b) => new PostWithDifficulty { Difficulty = a.Difficulty, Tags = b.Tags, Question = b.Question }).Where(a => a.Difficulty.Contains("Moderate")).ToList();
+                //        postdetails = dbView.ModeratePosts.Select(a => new PostWithDifficulty { PostId = a.PostId, Tags = a.Tags, Question = a.Question }).ToList();
+                //        break;
+                //    case 3:
+                //        userExpertise = "Expert";
+                //        maxPoints = 10000;
+                //        if (points[0] > 10000)
+                //            pointsMod = 10000;
+                //        else
+                //            pointsMod = points[0] - 500;
+                //        //postdetails = db.PostDetails.Join(db.Posts, a => a.PostId, b => b.PostId, (a, b) => new PostWithDifficulty { Difficulty = a.Difficulty, Tags = b.Tags, Question = b.Question }).Where(a => a.Difficulty.Contains("Hard")).ToList();
+                //        postdetails = dbView.HardPosts.Select(a => new PostWithDifficulty { PostId = a.PostId, Tags = a.Tags, Question = a.Question }).ToList();
+                //        break;
+                //}
+                //foreach (var item in splitTags)
+                //{
+                //    Boolean flag = false;
+                //    foreach (var i in postdetails)
+                //    {
+                //        if (i.Tags.ToLower().Contains(item.ToLower()))
+                //        {
+                //            var q = Regex.Match(i.Question.Trim(), "[^ ]*'Body':(.*), 'Title'").Groups[1].Value;
+                //            q = Regex.Replace(q, "['\"]", "");
+                //            q = Regex.Replace(q, "((\\\\r)+\\\\n)+", "<br />");
+                //            //q = Regex.Replace(q, "(\\\\r)+", "");
+                //            questions.Add(q);
+                //        }
+
+                //        if (questions.Count == 10)
+                //        {
+                //            flag = true;
+                //            break;
+                //        }
+                //    }
+
+                //    if (flag) break;
+
+                //}
+
+
                 foreach (var item in splitTags)
                 {
-                    Boolean flag = false;
+                    switch (expertise[0])
+                    {
+                        case 1:
+                            userExpertise = "Naive";
+                            //minPoints = 0;
+                            maxPoints = 100;
+                            pointsMod = points[0];
+                            //postdetails = db.PostDetails.Join(db.Posts, a => a.PostId, b => b.PostId, (a, b) => new PostWithDifficulty { Difficulty = a.Difficulty, Tags = b.Tags, Question = b.Question }).Where(a => a.Difficulty.Contains("Easy")).ToList();
+                            postdetails = dbView.EasyPosts.Where(a => a.Tags.ToLower().Contains(item.ToLower())).Select(a => new PostWithDifficulty { PostId = a.PostId, Tags = a.Tags, Question = a.Question }).Take(10).ToList();
+                            break;
+                        case 2:
+                            userExpertise = "Mediocre";
+                            maxPoints = 400;
+                            pointsMod = points[0] - 100;
+                            //postdetails = db.PostDetails.Join(db.Posts, a => a.PostId, b => b.PostId, (a, b) => new PostWithDifficulty { Difficulty = a.Difficulty, Tags = b.Tags, Question = b.Question }).Where(a => a.Difficulty.Contains("Moderate")).ToList();
+                            postdetails = dbView.ModeratePosts.Where(a => a.Tags.ToLower().Contains(item.ToLower())).Select(a => new PostWithDifficulty { PostId = a.PostId, Tags = a.Tags, Question = a.Question }).Take(10).ToList();
+                            break;
+                        case 3:
+                            userExpertise = "Expert";
+                            maxPoints = 10000;
+                            if (points[0] > 10000)
+                                pointsMod = 10000;
+                            else
+                                pointsMod = points[0] - 500;
+                            //postdetails = db.PostDetails.Join(db.Posts, a => a.PostId, b => b.PostId, (a, b) => new PostWithDifficulty { Difficulty = a.Difficulty, Tags = b.Tags, Question = b.Question }).Where(a => a.Difficulty.Contains("Hard")).ToList();
+                            postdetails = dbView.HardPosts.Where(a => a.Tags.ToLower().Contains(item.ToLower())).Select(a => new PostWithDifficulty { PostId = a.PostId, Tags = a.Tags, Question = a.Question }).Take(10).ToList();
+                            break;
+                    }
+
                     foreach (var i in postdetails)
                     {
-                        if (i.Tags.ToLower().Contains(item.ToLower()))
-                        {
-                            var q = Regex.Match(i.Question.Trim(), "[^ ]*'Body':(.*), 'Title'").Groups[1].Value;
-                            q = Regex.Replace(q, "['\"]", "");
-                            q = Regex.Replace(q, "((\\\\r)+\\\\n)+", "<br />");
-                            //q = Regex.Replace(q, "(\\\\r)+", "");
-                            questions.Add(q);
-                        }
+                        var q = Regex.Match(i.Question.Trim(), "[^ ]*'Body':(.*), 'Title'").Groups[1].Value;
+                        q = Regex.Replace(q, "['\"]", "");
+                        q = Regex.Replace(q, "((\\\\r)+\\\\n)+", "<br />");
+                        //q = Regex.Replace(q, "(\\\\r)+", "");
+                        questions.Add(q);
 
                         if (questions.Count == 10)
                         {
-                            flag = true;
                             break;
                         }
                     }
 
-                    if (flag) break;
-
+                    if (questions.Count == 10) break;
                 }
+
 
                 ViewBag.Name = username[0].Replace("\"", "");
                 ViewBag.Points = points[0];
@@ -131,14 +183,15 @@ namespace AdaptiveWebProject.Controllers
                 ViewBag.Upvotes = Int64.Parse(Regex.Replace(upvotes[0], "\\D", ""));
                 ViewBag.PointsMod = pointsMod;
                 ViewBag.MaxPoints = maxPoints;
+
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
                 RedirectToAction("Dashboard", "Home");
             }
             return View(questions);
         }
-        
+
 
     }
 }
